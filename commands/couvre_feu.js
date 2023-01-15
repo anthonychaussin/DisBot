103c2fd7ab7604900c5c4a26f60e1babe39c1a26
anthonyChaussin <chaussin.anthony@gmail.com>
2023-01-15 21:42:47 +0100

    fix time set

--- commands/couvre_feu.js
+++ commands/couvre_feu.js
@@ -49,14 +49,14 @@ module.exports = {
             const target = interaction.options.getUser('target');
             if (action === 'add') {
                 crons.push({
-                    cron: '0 ' + interaction.options.getInteger('minute') + ' ' + interaction.options.getInteger('heure') + ' * * *',
+                    cron: '0 ' + interaction.options.getInteger('minute') + ' ' + interaction.options.getInteger('heure')-1 + ' * * *',
                     action: 0,
                     author: author.username,
                     target: target,
                     guildId: interaction.guildId
                 })
                 crons.push({
-                    cron: '0 ' + interaction.options.getInteger('minute2') + ' ' + interaction.options.getInteger('heure2') + ' * * *',
+                    cron: '0 ' + interaction.options.getInteger('minute2') + ' ' + interaction.options.getInteger('heure2')-1 + ' * * *',
                     action: 1,
                     author: author.username,
                     target: target,
